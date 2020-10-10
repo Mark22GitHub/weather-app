4135ffe4a05504e083cfeee3a21fd313904ff5aa
Mark22GitHub <atelezhenko93@gmail.com>
2020-10-10 16:09:30 +0300

    weather data is ready

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,1 +1,31 @@
-const ApiKey = '184d35ee89a4d141bb8c25903bba881f'
+const ApiKey = '184d35ee89a4d141bb8c25903bba881f';
+
+export default {
+    _city: ``,
+    
+    fetchWeather() {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${ApiKey}`;
+      return fetch(url)
+        .then(response => response.json())
+
+
+    //     .then(res => {
+    //     const dt = new Date(res.dt * 1000)
+    //     refs.daysList.innerHTML= item({...res, dt})
+    // })
+      
+      
+      
+            // .then(data => {
+            //     console.log(data)
+            //     return data})
+    
+    },
+
+    get city () {
+    return this._city;
+  },
+  set city (newCity) {
+    this._city = newCity;
+  },
+}
--- src/js/renderApi.js
+++ src/js/renderApi.js
@@ -1,3 +1,22 @@
 import refs from "./refs.js";
-import fetch from "./fetch.js"
-import template from "../template/item.hbs";
+import fetch from "./fetch.js";
+import item from "../template/item.hbs";
+import moment from "moment"
+
+
+
+refs.form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  fetch.city = event.currentTarget.city.value
+  refs.input.value = "";
+  refs.daysList.innerHTML = "";
+
+  console.log(event.currentTarget.city.value);
+  fetch.fetchWeather().then(data => render({ data }));
+})
+function render(data) {
+    const templateItem = item(data);
+    refs.daysList.insertAdjacentHTML('beforeend', templateItem)
+    console.log(templateItem);
+}
